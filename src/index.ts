8ea4280464146b5e6caca543cb765c93ea458e69
juracdev <host@jurac.ru>
2023-04-11 18:33:23 +0300

    Parsing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { calculateTestsStats } from './answer-checking/calculateTestsStats';
 import { checkTestResults } from './answer-checking/checkAnswers';
 import { parseAnswerSheet } from './parsing/parseAnswerSheet';
+import { parseDocumentAnswers } from './parsing/parseDocumentAnswers';
+import { parseDocumentQuestions } from './parsing/parseDocumentQuestions';
 import { parseQuestionSheet } from './parsing/parseQuestionSheet';
+import { generateQuestionsSheet } from './test-generation/generateQuestionsSheet';
 import { generateTestForm } from './test-generation/generateTestForm';
 
 function main() {}
@@ -19,4 +22,16 @@ function checkAns() {
   calculateTestsStats(testResults);
 
   console.log(JSON.stringify(testResults));
+}
+
+function parseDocument(docId: string) {
+  const doc = DocumentApp.openById(docId);
+  const body = doc.getBody();
+
+  const questions = parseDocumentQuestions(body);
+  parseDocumentAnswers(body, questions);
+
+  console.log(JSON.stringify(questions));
+
+  generateQuestionsSheet(questions);
 }
